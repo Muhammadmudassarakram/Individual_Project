a5326dfa1f44c09bc514d37a207e3c51035ef018
Muhammadmudassarakram <55785715+Muhammadmudassarakram@users.noreply.github.com>
2020-09-24 18:23:06 +0200

    only for deploying

--- pages/_app.js
+++ pages/_app.js
@@ -44,12 +44,11 @@ return(
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous" ></script>
  </Head>
 
-  <NaveBar isAuthenticated={auth} />
-  <div className="base-page">
-  <Component {...pageProps}  auth={auth}/>
- </div>
- <Footer/>
-  
+      <NaveBar isAuthenticated={auth} />
+        <div className="base-page">
+        <Component {...pageProps}  auth={auth}/>
+        </div>
+    <Footer/>
 </div>
 
 )
